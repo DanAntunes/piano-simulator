9395587f12eeb7a612086cacd1e694ce06ee0ed9
Soren <danilo-258@hotmail.com>
2024-11-27 13:01:44 -0300

    feat(js): adicionar controle de volume para o piano virtual
    - Implementado controle de volume global usando um input range.
    - Adicionada lógica para ajustar o volume dos áudios baseando-se no valor selecionado no slider.
    - Volume é aplicado dinamicamente para todas as notas tocadas, tanto via clique quanto via teclado.
    - Integrado o evento `input` ao controle de volume para atualização em tempo real.

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,10 +1,12 @@
-const pianoKeys = document.querySelectorAll(".piano-keys .key")
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
 
-// biome-ignore lint/style/useConst: <explanation>
-let audio = new Audio("src/assets/tunes/a.wav");
+const keyMapping = [];
+let globalVolume = 0.5; 
 
 const playTune = (key) => {
   const audio = new Audio(`src/assets/tunes/${key}.wav`);
+  audio.volume = globalVolume;
   audio.play();
 
   const clickedKey = document.querySelector(`[data-key="${key}"]`);
@@ -14,12 +16,19 @@ const playTune = (key) => {
   }, 150);
 };
 
+const handleVolume = (e) => {
+  globalVolume = e.target.value;
+};
+volumeSlider.addEventListener("input", handleVolume);
+
 // biome-ignore lint/complexity/noForEach: <explanation>
-pianoKeys.forEach((key)=>{
-  console.log(key.dataset.key);
-  key.addEventListener("click",()=> playTune(key.dataset.key));
+pianoKeys.forEach((key) => {
+  key.addEventListener("click", () => playTune(key.dataset.key));
+  keyMapping.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
-  playTune(e.key);
+  if (keyMapping.includes(e.key)) {
+    playTune(e.key);
+  }
 });
